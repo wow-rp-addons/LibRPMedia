a7c6edc24154528932308cafa4d8b9ec59155e42
Daniel Yates <me@meorawr.io>
2019-08-18 12:31:47 +0000

    Blacklist broken Classic icons

--- Exporter/Config/Classic.lua
+++ Exporter/Config/Classic.lua
@@ -17,6 +17,36 @@ return {
         excludeNames = {
             -- "Blizzard" branded icons.
             "^mail_gmicon$",
+
+            -- Invisible icons.
+            "^inv_mace_18$",
+            "^inv_staff_37$",
+
+            -- Icons that are clearly Retail-only and show up in the filelist
+            -- for classic and are perfectly downloadable, but the client
+            -- doesn't actually have because Classic is probably some sort of
+            -- weird hackjob.
+            "^ability_blackhand_",
+            "^ability_deathwing_",
+            "^ability_racial_viciousness",
+            "^achievement_character_human_",
+            "^achievement_dungeon_naxxramas",
+            "^garr_currencyicon",
+            "^inv_drink_31_embalmingfluid$",
+            "^inv_misc_bone_humanskull_02$",
+            "^inv_misc_bone_skull_01$",
+            "^inv_misc_eye_02$",
+            "^inv_misc_eye_04$",
+            "^inv_misc_firedancer_01$",
+            "^inv_misc_food_meat_raw_07$",
+            "^inv_misc_head_nerubian_01$",
+            "^quest_12252_icon$",
+            "^sha_inv_misc_",
+            "^spell_arcane_teleporthalloftheguardian",
+            "^spell_deathknight_",
+            "^spell_shadow_brainwash",
+            "^xp_icon$",
+            "^xpbonus_icon$",
         },
     },
 
