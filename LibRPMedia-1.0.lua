3f48e8032a9d4c3b249ff9c73154d8488f1af6c3
Daniel Yates <me@meorawr.io>
2022-08-23 22:30:46 +0000

    Bump library version

--- LibRPMedia-1.0.lua
+++ LibRPMedia-1.0.lua
@@ -2,7 +2,7 @@
 assert(LibStub, "Missing dependency: LibStub");
 
 local MODULE_MAJOR = "LibRPMedia-1.0";
-local MODULE_MINOR = 14;
+local MODULE_MINOR = 15;
 
 local LibRPMedia = LibStub:NewLibrary(MODULE_MAJOR, MODULE_MINOR);
 if not LibRPMedia then
