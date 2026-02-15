00bcd238042d439524f1d6d67fc340b69793b602
GitHub <noreply@github.com>
2026-02-15 01:35:09 +0000

    Regenerate Databases

--- LibRPMedia-1.2.lua
+++ LibRPMedia-1.2.lua
@@ -25,7 +25,7 @@
 
 assert(LibStub, "Missing dependency: LibStub");
 
-local MINOR_VERSION = 987654321;
+local MINOR_VERSION = 987654322;
 
 local LRPM12 = LibStub:NewLibrary("LibRPMedia-1.2", MINOR_VERSION);
 
